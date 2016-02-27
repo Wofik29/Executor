dfa6daab66eb868585a45cadc5193a6d2b499377
Wolf <Linad29@mail.ru>
2016-02-27 10:35:06 +0600

    Текстурный атлас
    Сделал текстурный атлас и его адекватную загрузку. HashMap с текстурными
    координатами, по ключу на циферное обозначение текстуры.

--- src/Game/MainTest.java
+++ src/Game/MainTest.java
@@ -1,13 +1,22 @@
 package Game;
 
+import java.util.HashMap;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Compiller c = new Compiller();
-		c.setFile("test.txt");
-		c.read();
-		c.getProgramm();
+		HashMap< Integer, String> map = new HashMap<>();
+		
+		map.put(0, "Zero");
+		map.put(1, "One");
+		map.put(2, "Two");
+		map.put(3, "Three");
+		map.put(4, "Four");
+		
+		for (int i=0; i<5; i++)
+			System.out.println(map.get(i));
+		
 	}
 
 }
--- src/Game/Window.java
+++ src/Game/Window.java
@@ -3,6 +3,7 @@ package Game;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.List;
 import javax.swing.Timer;
 
@@ -27,20 +28,20 @@ public class Window implements Runnable
 	int fps;
 	int width, height;
 	
+	float tex_width;
+	float tex_height;
+	
 	byte[][] map;
 	//Font awtfont = new Font("Times New Roman", Font.BOLD, 25);
 	TrueTypeFont font;
 	int step;
 	int rot = 0;
 	volatile List<GameObject> objects;
-	Texture grass;
-	Texture beach;
-	Texture grass_beach;
-	Texture beach_water;
-	Texture[] water = new Texture[2];
-	Texture water_deep;
-	Texture deep;
+
+	HashMap<Integer , int[]> coordTex = new HashMap<Integer, int[]>(50);
 	
+	
+	Texture sprites;
 	
 	Controller controller;
 	Timer timer;
@@ -52,6 +53,8 @@ public class Window implements Runnable
 	{
 		width = w;
 		height = h;
+		tex_height = 1f/8f;
+		tex_width = 1f/8f;
 		this.step = step;
 		controller = c;
 		timer = new Timer(200, new ActionListener() {
@@ -60,9 +63,53 @@ public class Window implements Runnable
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				i_w = (i_w==0) ? (i_w=1) : (i_w=0);
-				System.out.println(water[i_w].getTextureID());
+			
 			}
 		});
+		
+		
+		
+		coordTex.put(0, new int[]{5,0}); // grass
+		coordTex.put(1, new int[]{0,0}); // deep
+		coordTex.put(2, new int[]{1,4}); // beach
+		coordTex.put(3, new int[]{2,3}); // shallow
+		coordTex.put(4, new int[]{5,2}); // grass-beach-in-1
+		coordTex.put(5, new int[]{0,3}); // grass-beach-in-2
+		coordTex.put(6, new int[]{1,3}); // grass-beach-in-3
+		coordTex.put(7, new int[]{4,3}); // grass-beach-in-4
+		coordTex.put(8, new int[]{5,4}); // grass-beach-out-1
+		coordTex.put(9, new int[]{7,3}); // grass-beach-out-1
+		coordTex.put(10, new int[]{5,4}); // grass-beach-out-2
+		coordTex.put(11, new int[]{7,2}); // grass-beach-out-3
+		coordTex.put(12, new int[]{3,4}); // grass-beach-out-4
+		coordTex.put(13, new int[]{6,2}); // grass-beach-1
+		coordTex.put(14, new int[]{3,3}); // grass-beach-2
+		coordTex.put(15, new int[]{6,3}); // grass-beach-3
+		coordTex.put(16, new int[]{7,4}); // grass-beach-4
+		coordTex.put(17, new int[]{2,0}); // beach-shallow-in-1
+		coordTex.put(18, new int[]{2,4}); // beach-shallow-in-2
+		coordTex.put(19, new int[]{6,0}); // beach-shallow-in-3
+		coordTex.put(20, new int[]{7,0}); // beach-shallow-in-4
+		coordTex.put(21, new int[]{0,4}); // beach-shallow-out-1
+		coordTex.put(22, new int[]{4,0}); // beach-shallow-out-2
+		coordTex.put(23, new int[]{6,4}); // beach-shallow-out-3
+		coordTex.put(24, new int[]{0,1}); // beach-shallow-out-4
+		coordTex.put(25, new int[]{1,0}); // shallow-deep-in-1
+		coordTex.put(26, new int[]{0,2}); // shallow-deep-in-2
+		coordTex.put(27, new int[]{2,1}); // shallow-deep-in-3
+		coordTex.put(28, new int[]{1,1}); // shallow-deep-in-4
+		coordTex.put(29, new int[]{7,1}); // shallow-deep-out-1
+		coordTex.put(30, new int[]{1,2}); // shallow-deep-out-2
+		coordTex.put(31, new int[]{2,2}); // shallow-deep-out-3
+		coordTex.put(32, new int[]{6,1}); // shallow-deep-out-4
+		coordTex.put(33, new int[]{4,1}); // shallow-deep-1
+		coordTex.put(34, new int[]{5,1}); // shallow-deep-2
+		coordTex.put(35, new int[]{3,1}); // shallow-deep-3
+		coordTex.put(36, new int[]{4,0}); // shallow-deep-4
+		coordTex.put(37, new int[]{4,2}); // beach-shallow-1
+		coordTex.put(38, new int[]{7,3}); // beach-shallow-2
+		coordTex.put(39, new int[]{4,4}); // beach-shallow-3
+		coordTex.put(40, new int[]{7,4}); // beach-shallow-4
 		
 	}
 	
@@ -112,33 +159,13 @@ public class Window implements Runnable
 
 		try
 		{
-			grass = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/4.png"));
-			beach = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/2.png"));
-			grass_beach = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/0.png"));
-			beach_water = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/3.png"));
-			Texture tex = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/1.png"));
-			water[0] = tex;
-			System.out.println(tex.getTextureID());
-			System.out.println(water[0].getTextureID());
-			tex = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/8.png"));
-			water[1] = tex;
-			System.out.println(tex.getTextureID());
-			System.out.println(water[1].getTextureID());
-			water_deep = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/7.png"));
-			deep = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/6.png"));
 			
+			sprites = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/Spritesheet.png"));
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		
-		/*
-		GL11.glMatrixMode(GL11.GL_PROJECTION);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 800, 0, 600, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		*/
 	}
 		
 	private void drawQuad(GameObject q)
@@ -163,15 +190,16 @@ public class Window implements Runnable
 		GL11.glPushMatrix();
 		GL11.glTranslatef(trans, 0, 0);
 		GL11.glScalef(scal,scal, 0);
-		Texture t = null;
 		
 		float Xo = 0;
 	    float Yo = 0;
 	    int height = 32;
 	    int width = 64;
 	    float C =(float) Math.floor(Display.getWidth()/ 2);
-	    
+	    int [] t = {0,0};
 	    float Xc = 0;
+	   // System.out.println();
+	   // System.out.println("=====");
 		
 		for (int y=0; y<map.length; y++)
 		{
@@ -185,26 +213,17 @@ public class Window implements Runnable
 			{
 				Xo = Xc + (x * (width / 2));
 				Yo += height / 2;
-				switch (map[y][x])
-				{
-				case 0:	t = grass_beach; break;
-				case 1: t = water[i_w]; break;
-				case 2: t = beach; break;
-				case 3: t = beach_water; break;
-				case 4: t = grass; break;
-				case 6: t = deep; break;
-				case 7: t = water_deep; break;
-				}
 				
+				t = coordTex.get((int)map[y][x]);
+				//System.out.println(map[y][x]);
+				//System.out.print(" "+t[0]+" , ");
 				
-				
-				drawTexture(Xo, Yo, 0, t);
+				drawTexture(Xo, Yo, 0, t[0], t[1]);
 				
 			}
+			System.out.println();
 			
 		}
-		
-		
 		
 		GL11.glPopMatrix(); 
 	}
@@ -246,36 +265,48 @@ public class Window implements Runnable
 	    
 	    return delta;
 	}
-	
-	public void update(int delta) 
-	{
-		
-	}
-	
-	void drawTexture(float x, float y, float z, Texture t)
+
+	void drawTexture(float px, float py, float pz, float tx, float ty)
 	{
 		GL11.glPushMatrix();
-		GL11.glTranslatef(x*t.getHeight(), y*t.getWidth(), z);
+		GL11.glTranslatef(px, py, pz);
 		
 		//GL11.glRotatef(rot, 0f, 0f, 1f);
 		
-		//Color.white.bind();
-        t.bind(); // or GL11.glBind(texture.getTextureID());
-        
+		Color.white.bind();
+        //t.bind(); // or GL11.glBind(texture.getTextureID());
+ /*
         GL11.glBegin(GL11.GL_QUADS);
             GL11.glTexCoord2f(0,0);
+            GL11.glVertex2f(-64,-32);
+            
+            GL11.glTexCoord2f(0.25f,0);
+            GL11.glVertex2f(64, -32);
+            
+            GL11.glTexCoord2f(0.25f,0.09f);
+            GL11.glVertex2f(64, 32);
+           
+            GL11.glTexCoord2f(0,0.09f);
+            GL11.glVertex2f(-64, 32);
+        GL11.glEnd();
+    */     
+		//System.out.println(ty);
+		//System.out.println(tex_height);
+		//System.out.println(ty*tex_height);
+        GL11.glBegin(GL11.GL_QUADS);
+            GL11.glTexCoord2f(tx*tex_width, ty*tex_height);
             GL11.glVertex2f(-32,-32);
             
-            GL11.glTexCoord2f(1f,0);
+            GL11.glTexCoord2f(tx*tex_width+tex_width, ty*tex_height);
             GL11.glVertex2f(32, -32);
             
-            GL11.glTexCoord2f(1f,1f);
+            GL11.glTexCoord2f(tx*tex_width+tex_width, ty*tex_height+tex_height);
             GL11.glVertex2f(32, 32);
-            
-            GL11.glTexCoord2f(0,1f);
+           
+            GL11.glTexCoord2f(tx*tex_width, ty*tex_height+tex_height);
             GL11.glVertex2f(-32, 32);
         GL11.glEnd();
-        GL11.glEnable(GL11.GL_TEXTURE_BINDING_2D);
+       
         GL11.glPopMatrix();
 	}
 	
@@ -289,13 +320,22 @@ public class Window implements Runnable
 		//System.out.println(grass.getTextureHeight());
 		//System.out.println(rot);
 		
+		//for (int i = 0; i<41;i++)
+		//{
+		//	int[] t = coordTex.get(i);
+		//	drawTexture(100+64*i, 100, 0, t[0], t[1]);
+		//}
+		
+		
+		//System.out.println("0: "+t[0]+", 1: "+t[1]);
+		//drawTexture(100, 200, 0, beach_water);
 		if (map != null) 
 		{
 			drawMap();
 			if (objects != null)
 				for (GameObject q : objects)
 				{
-					drawQuad(q);
+					//drawQuad(q);
 				}
 		}
 		else
@@ -335,6 +375,14 @@ public class Window implements Runnable
 		getDelta();
 		initGL();
 		timer.start();
+		sprites.bind();
+		
+		for (int key: coordTex.keySet())
+		{
+			int t[] = coordTex.get(key);
+			System.out.println(key+" : "+t[0]+" , "+t[1]);
+		}
+		
 		while (!Display.isCloseRequested())
 		{
 			inputLoop();
