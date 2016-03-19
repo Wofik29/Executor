10749329322f6a13db2ad25a9a6da3dba0f2f32a
Wolf <Linad29@mail.ru>
2016-03-19 21:44:50 +0500

    Релизовал логику класса ifTerm

--- src/Game/ifTerm.java
+++ src/Game/ifTerm.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.awt.Point;
+
 public class ifTerm extends Queue 
 {
 	/* 
@@ -12,9 +14,7 @@ public class ifTerm extends Queue
 	private int term2 = -1;
 
 	// Номер элемента, с которого начинется ветка else. 
-	private int number_else;
-	
-	private boolean entry = false;
+	private int number_else = -1;
 	
 	public void setTerm1(int t1)
 	{
@@ -43,7 +43,23 @@ public class ifTerm extends Queue
 	{
 		if (current_command != null)
 		{
-			// Если мы в начале, то надо сделать проверку условия. 
+			Point[][] dir = new Point[4][3];
+			dir[0][0] = new Point(0,1);
+			dir[0][1] = new Point(1,0);
+			dir[0][2] = new Point(0,-1);
+			dir[1][0] = new Point(-1,0);
+			dir[1][1] = dir[0][0];
+			dir[1][2] = dir[0][1];
+			dir[2][0] = dir[0][2];
+			dir[2][1] = dir[1][0];
+			dir[2][2] = dir[0][0];
+			dir[3][0] = dir[0][1];
+			dir[3][1] = dir[0][2];
+			dir[3][2] = dir[1][0];
+					
+			/*
+			 * Если первый раз зашли, то делаем проверку на условие
+			 */
 			if (current_number == 0)
 			{
 				/*
@@ -52,6 +68,12 @@ public class ifTerm extends Queue
 				 */
 				int x=-1;
 				int y=-1;
+				
+				Point p = dir[obj.direction][term1];
+				
+				x = obj.x+p.x;
+				y = obj.y+p.y;
+				/*
 				switch (obj.direction)
 				{
 				case 0: // down-left.
@@ -70,28 +92,29 @@ public class ifTerm extends Queue
 					
 					break;
 				}
-				
+				*/
 				int l = obj.map.length;
 				if (x >= 0 && x<l && y>=0 && y<=l )
 				{
-					
+					if (obj.map[x][y] != term2)
+					{
+						if (number_else>-1)
+							current_number = number_else;							
+						else
+							isEnd = false;
+					}
 				}
+			}
+			
+			if (isEnd && current_command.execute(obj))
+			{
+				// TODO Тут возможно ошибка, когда последний элемент в списке, тогда один ход будет пропущен
+				next();
+				return false;
 			}
 		}
 		
-		if (isEnd && current_command != null && current_command.execute(obj))
-		{
-			//System.out.println("execute "+current_command.toString()+" - "+current_number);
-			
-			next();
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-		
-	
+		return true;
 	}
 	
 	private void next()
