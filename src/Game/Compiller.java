c63ad0c072fbe2e99a3e28649e5d3060e0c3532a
Wolf <Linad29@mail.ru>
2016-03-19 21:44:22 +0500

    Перенес создание программы в класс Compiller

--- src/Game/Compiller.java
+++ src/Game/Compiller.java
@@ -55,6 +55,8 @@ public class Compiller
 		//System.out.println(sb);
 	}
 	
+	
+	
 	public void getProgramm2()
 	{
 		char[] s = sb.toString().toCharArray();
@@ -120,6 +122,134 @@ public class Compiller
 				}
 			}
 		}
+	}
+	
+	public Queue getProgramm(String text)
+	{
+		//StringBuilder sb = new StringBuilder(text);
+		/*
+		*  0 - обычное
+		*  1 - обработка условия для цикла
+		*/
+		int state = 0;
+		boolean error = false;
+		String error_text = "";
+		
+		Stack<Queue> stack = new Stack<>();
+		Queue programm = new MainLoop();
+		Queue current = programm;
+				
+		String[] lines = text.split("\t");
+				
+		parse: for (String line: lines)
+		{
+			String[] strs = line.split("\\s+");
+			
+			/* 
+			 * Проверяем строку и на основании нее выбираем вид состояния
+			 */
+			for (String str : strs )
+			{
+				// TODO разбивать на буквы и по буквенно проверять. например ahead=lefty будет как одна строка, надо вычленить каждый term
+						
+				if (str.charAt(0) == '(')
+					str = str.substring(1);
+				if (str.charAt(str.length()-1) == ')')
+					str = str.substring(0,str.length()-1);
+				System.out.println(str);
+			
+				switch (state)
+				{
+				// Обычное, добавляем команды
+				case 0:
+					switch (str.toLowerCase())
+					{
+					case "forward":
+						current.add(new Forward());
+						break;
+					case "left":
+						current.add(new Left());
+						break;
+					case "right":
+						current.add(new Right());
+						break;
+					case "if":
+						state = 1;
+						stack.push(current);
+						Queue temp = new ifTerm(); 
+						current.add(temp);
+						current = temp;
+						temp = null;
+						break;
+					case "end":
+						current = stack.pop();
+						break;
+					case "else":
+						ifTerm if_temp = (ifTerm) current;
+						if_temp.setElse();
+						if_temp = null;
+						break;
+					default: 
+						error = true;
+						error_text = "Ожидался оператор, но встречен "+str;
+						break parse;
+					}
+					break;
+				case 1:
+					ifTerm if_temp = (ifTerm) current;
+					switch (str.toLowerCase())
+					{
+					case "ahead":
+						if_temp.setTerm1(1);
+						break;
+					case "lefty":
+						if_temp.setTerm1(0);
+						break;
+					case "righty":
+						if_temp.setTerm1(2);
+						break;
+					case "water":
+						if_temp.setTerm2(Map.SHALLOW);
+						break;
+					case "beach":
+						if_temp.setTerm2(Map.BEACH);
+						break;
+					case "then": 
+						if (if_temp.isAllTerm()) state = 0;
+						else
+						{
+							error = true;
+							error_text = "Условие не полное!";
+							break parse;
+						}
+						// TODO Проверка, полностью ли написано условие.
+								
+						break;
+					default:
+						error = true;
+						error_text = "Ожидалось условие, но встречен "+str;
+						// TODO вывод ошибки "Ожидалось условие, но нашел str
+						break parse;
+					}
+				case 2:
+					switch (str.toLowerCase())
+					{
+						
+					}
+					break;
+				case 3:
+					break;
+				}
+			}
+		}
+				
+		if (error)
+		{
+			System.out.println(error_text);
+			// TODO скорее всего будет throw exeption, и написание ошибки.
+		}
+		
+		return programm;
 	}
 	
 	public Queue getProgramm1()
--- src/Game/Controller.java
+++ src/Game/Controller.java
@@ -32,134 +32,9 @@ public class Controller
 	
 	public void setProgramm(String text)
 	{
-		
-		//StringBuilder sb = new StringBuilder(text);
-		/*
-		 *  0 - обычное
-		 *  1 - обработка условия для цикла
-		 */
-		int state = 0;
-		boolean error = false;
-		String error_text = "";
-		
-		
-		Stack<Queue> stack = new Stack<>();
-		
-		Queue programm = new MainLoop();
-		Queue current = programm;
-		
-		String[] lines = text.split("\t");
-		
-		parse: for (String line: lines)
-		{
-			String[] strs = line.split("\\s+");
-			
-			/* 
-			 * Проверяем строку и на основании нее выбираем вид состояния
-			 */
-			for (String str : strs )
-			{
-				// TODO разбивать на буквы и по буквенно проверять. например ahead=lefty будет как одна строка, надо вычленить каждый term
-				
-				if (str.charAt(0) == '(')
-					str = str.substring(1);
-				if (str.charAt(str.length()-1) == ')')
-					str = str.substring(0,str.length()-1);
-				System.out.println(str);
-				
-				
-				switch (state)
-				{
-				// Обычное, добавляем команды
-				case 0:
-					switch (str.toLowerCase())
-					{
-					case "forward":
-						current.add(new Forward());
-						break;
-					case "left":
-						current.add(new Left());
-						break;
-					case "right":
-						current.add(new Right());
-						break;
-					case "if":
-						state = 1;
-						stack.push(current);
-						Queue temp = new ifTerm(); 
-						current.add(temp);
-						current = temp;
-						temp = null;
-						break;
-					case "end":
-						current = stack.pop();
-						break;
-					case "else":
-						ifTerm if_temp = (ifTerm) current;
-						if_temp.setElse();
-						if_temp = null;
-						break;
-					default: 
-						error = true;
-						error_text = "Ожидался оператор, но встречен "+str;
-						break parse;
-					}
-					break;
-				case 1:
-					ifTerm if_temp = (ifTerm) current;
-					switch (str.toLowerCase())
-					{
-					case "ahead":
-						if_temp.setTerm1(1);
-						break;
-					case "lefty":
-						if_temp.setTerm1(0);
-						break;
-					case "righty":
-						if_temp.setTerm1(2);
-						break;
-					case "water":
-						if_temp.setTerm2(Map.SHALLOW);
-						break;
-					case "beach":
-						if_temp.setTerm2(Map.BEACH);
-						break;
-					case "then": 
-						if (if_temp.isAllTerm()) state = 0;
-						else
-						{
-							error = true;
-							error_text = "Условие не полное!";
-							break parse;
-						}
-						// TODO Проверка, полностью ли написано условие.
-						
-						break;
-					default:
-						error = true;
-						error_text = "Ожидалось условие, но встречен "+str;
-						// TODO вывод ошибки "Ожидалось условие, но нашел str
-						break parse;
-					}
-				case 2:
-					switch (str.toLowerCase())
-					{
-					
-					}
-					break;
-				case 3:
-					break;
-				}
-			}
-		}
-		
-		if (error)
-		{
-			System.out.println(error_text);
-			// TODO скорее всего будет throw exeption, и написание ошибки.
-		}
-		
-		world.getObjects().get(0).setProgramm((MainLoop)current);
+		Compiller c = new Compiller();
+		Queue prog = c.getProgramm(text);
+		world.getPlayer().setProgramm( (MainLoop) prog);
 	}
 	
 	public void pressedKey(int key, char c)
@@ -215,16 +90,9 @@ public class Controller
 		//thread_window.start();
 		//EventQueue.invokeLater(window);
 		thread_world.start();
-		try
-		{
-			//thread_window.join();
-		}
-		catch (Exception ex)
-		{
-			ex.printStackTrace();
-		}
 		
-		//world.stop();
+		
+		//	world.stop();
 	}
 	
 	
