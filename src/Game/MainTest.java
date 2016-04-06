26593a455523ab5c4d8b78b13416c7523d1e33f5
Wofik29 <Linad29@mail.ru>
2016-04-06 14:06:59 +0500

    Сделал тест на проверку правильности записи карты.

--- src/Game/MainTest.java
+++ src/Game/MainTest.java
@@ -5,6 +5,26 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		MainTest test = new MainTest();
+		test.test_map();
+		
+		
+	}
+	
+	public void test_map()
+	{
+		Map map = new Map();
+		byte[][] m = map.loadMap();
+		
+		for (int i=0; i<m.length; i++)
+		{
+			
+			for (int j=0; j<m[i].length; j++)
+			{
+				System.out.print(m[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
