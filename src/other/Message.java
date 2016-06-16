d9cd27ba9117bbaae63d4bd58de4b90e870fdb1b
Wolf <Linad29@mail.ru>
2016-06-16 10:12:28 +0500

    Мега рефакторинг 3. Вспомогательные классы.
    Два класса для передачи сообщений между клиентами и сервером.

---
+++ src/other/Message.java
@@ -0,0 +1,57 @@
+package other;
+
+import java.io.Serializable;
+
+public class Message implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	/*
+	 * Типы сообщений
+	 * от всех:
+	 * exit - означает, что клиент/сервер вышел.
+	 * register - регистрация клиента
+	 * programm - текст алгоритма
+	 * 
+	 * только от сервера:
+	 * map - пересылает карту 
+	 * step - текущее положение корабликов 
+	 * message - обычное оповещение 
+	 * addPlayer - добавление нового игрока 
+	 * deletePlayer - игрок вышел
+	 * error - произошла ошибка 
+	 */
+	public String type = "";
+	public SPlayer player = null;
+	public String name = "";
+	public String text = "";
+	public SPlayer[] players = new SPlayer[2];
+	public int size = 0;
+	public byte[][] map;
+	
+	public Message(String name, String type, String text)
+	{
+		this.name = name;
+		this.type = type;
+		this.text = text;
+		player = null;
+	}
+	
+	public Message(String type)
+	{
+		this.type = type;
+	}
+	
+	public Message()
+	{
+		name = "";
+		type = "";
+		text = "";
+		player = null;
+	}
+	
+	public void addPlayer(SPlayer p)
+	{
+		if (size<5)
+			players[size++] = p;
+	}
+}
